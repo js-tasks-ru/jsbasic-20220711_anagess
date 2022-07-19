7432c2de8bae2cb3d5a8f66e83782dbd454ef894
Anna Parshakova <anagess@yandex.ru>
2022-07-19 14:51:41 +0300

    Первая задача второго модуля, суммы зп

--- 2-module/1-task/index.js
+++ 2-module/1-task/index.js
@@ -1,3 +1,14 @@
 function sumSalary(salaries) {
-  // ваш код...
+  let arraySal = [];
+  for (let prop in salaries) {
+    if (
+      typeof salaries[prop] === "number" &&
+      isFinite(salaries[prop]) &&
+      !isNaN(salaries[prop])
+    ) {
+      arraySal.push(salaries[prop]);
+    }
+  }
+  let result = arraySal.reduce((sum, current) => sum + current, 0);
+  return result;
 }
