39ca1a7b34cf85ecae51b0fdadcc1598d5c8eba2
Anna Parshakova <anagess@yandex.ru>
2022-07-20 17:19:40 +0300

    Исправила первую задачу и решила две оставшиеся

--- 2-module/1-task/index.js
+++ 2-module/1-task/index.js
@@ -1,14 +1,9 @@
 function sumSalary(salaries) {
   let arraySal = [];
   for (let prop in salaries) {
-    if (
-      typeof salaries[prop] === "number" &&
-      isFinite(salaries[prop]) &&
-      !isNaN(salaries[prop])
-    ) {
+    if (typeof salaries[prop] === "number" && isFinite(salaries[prop])) {
       arraySal.push(salaries[prop]);
     }
   }
-  let result = arraySal.reduce((sum, current) => sum + current, 0);
-  return result;
+  return arraySal.reduce((sum, current) => sum + current, 0);
 }
--- 2-module/3-task/index.js
+++ 2-module/3-task/index.js
@@ -1,6 +1,14 @@
 let calculator = {
-  // ваш код
+  read(a, b) {
+    this.firstNumber = a;
+    this.secondNumber = b;
+  },
+  sum(a, b) {
+    return this.firstNumber + this.secondNumber;
+  },
+  mul(a, b) {
+    return this.firstNumber * this.secondNumber;
+  },
 };
-
 // НЕ УДАЛЯТЬ СТРОКУ, НУЖНА ДЛЯ ПРОВЕРКИ
 window.calculator = calculator; // делает ваш калькулятор доступным глобально
