cd60635f0b864276073a36e206427702c83cd639
Anna Parshakova <anagess@yandex.ru>
2022-07-28 09:41:02 +0300

    исправлен весь третий модуль и первая задача четвертого

--- 4-module/1-task/index.js
+++ 4-module/1-task/index.js
@@ -1,6 +1,5 @@
 function makeFriendsList(friends) {
   const ul = document.createElement("ul");
-  document.body.append(ul);
   for (value of friends) {
     let li = document.createElement("li");
     li.innerHTML = value.firstName + " " + value.lastName;
