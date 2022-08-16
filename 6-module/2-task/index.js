ac40f7f9111794495e6e98a068680550c928dd1b
Anna Parshakova <Parsh723@mail.ru>
2022-08-16 15:16:34 +0300

    исправила 6ой модуль

--- 6-module/2-task/index.js
+++ 6-module/2-task/index.js
@@ -1,29 +1,36 @@
+import createElement from "../../assets/lib/create-element.js";
 
 export default class ProductCard {
   constructor(product) {
-  let div = document.createElement("div");
-  div.classList.add("card");
-  
-  let result = `<div class="card__top">
-  <img src="/assets/images/products/${product.image}" class="card__image" alt="product">
+    this.elem = this.#render(product);
+    const button = this.elem.querySelector("#" + product.id);
+    this.id = button.getAttribute("id");
+    button.addEventListener("click", this.#onProductAddClick);
+  }
+  #render(product) {
+    const element = createElement(`<div class="card">`);
+    const cardTop = createElement(`<div class="card__top">
+  <img src="/assets/images/products/${
+    product.image
+  }" class="card__image" alt="product">
   <span class="card__price">€${product.price.toFixed(2)}</span>
-</div>
+</div>`);
+    const cardBody = createElement(`
 <div class="card__body">
   <div class="card__title">${product.name}</div>
   <button id = "${product.id}" type="button" class="card__button">
     <img src="/assets/images/icons/plus-icon.svg" alt="icon">
   </button>
-</div>`;
-div.innerHTML = result;
-this.elem = div;
-
-function createEvent(){
-let event = new CustomEvent("product-add", {
-  detail: product.id,
-  bubbles: true,
-  }); 
-div.dispatchEvent(event)}
-let button = div.querySelector("#"+product.id);
-button.addEventListener('click', createEvent);
-}
+</div>`);
+    element.append(cardTop);
+    element.append(cardBody);
+    return element;
+  }
+  #onProductAddClick = () => {
+    const event = new CustomEvent("product-add", {
+      detail: this.id,
+      bubbles: true,
+    });
+    document.body.querySelector(".card").dispatchEvent(event);
+  };
 }
--- 6-module/3-task/index.js
+++ 6-module/3-task/index.js
@@ -1,25 +1,39 @@
-import createElement from '../../assets/lib/create-element.js';
+import createElement from "../../assets/lib/create-element.js";
 
 export default class Carousel {
- constructor(slides) {
-    this.slides = slides;
-    const elem = createElement(`<div class="carousel"></div>`);  
+  positionLenta = 0;
 
- let right = createElement(`<div class="carousel__arrow carousel__arrow_right">
+  constructor(slides) {
+    this.slides = slides;
+    this.elem = this.#render(slides);
+    const leftButton = this.elem.querySelector(".carousel__arrow_left");
+    const rightButton = this.elem.querySelector(".carousel__arrow_right");
+    leftButton.style.display = "none";
+    rightButton.addEventListener("click", this.#onRightArrowClick);
+    leftButton.addEventListener("click", this.#onLeftArrowClick);
+    let buttons = this.elem.querySelectorAll(".carousel__button");
+    for (let i = 0; i < buttons.length; i++) {
+      buttons[i].addEventListener("click", this.#onProductAddClick);
+    }
+  }
+  #render(slides) {
+    const elem = createElement(`<div class="carousel"></div>`);
+    let right =
+      createElement(`<div class="carousel__arrow carousel__arrow_right">
    <img src="/assets/images/icons/angle-icon.svg" alt="icon">
- </div>`)
- let left = createElement(`<div class="carousel__arrow carousel__arrow_left">
+ </div>`);
+    let left = createElement(`<div class="carousel__arrow carousel__arrow_left">
    <img src="/assets/images/icons/angle-left-icon.svg" alt="icon">
  </div>`);
- elem.append(right);
- elem.append(left);
- 
+    elem.append(right);
+    elem.append(left);
     let carouselInner = createElement(`<div class="carousel__inner">`);
-  
     for (let slide of slides) {
       let newSlide = createElement(`
       <div class="carousel__slide" data-id="${slide.id}">
-             <img src="/assets/images/carousel/${slide.image}" class="carousel__img" alt="slide">
+             <img src="/assets/images/carousel/${
+               slide.image
+             }" class="carousel__img" alt="slide">
              <div class="carousel__caption">
                <span class="carousel__price">€${slide.price.toFixed()}</span>
                <div class="carousel__title">${slide.name}</div>
@@ -28,60 +42,50 @@ export default class Carousel {
                </button>
              </div>
              </div>`);
-        carouselInner.append(newSlide);    
-elem.append(carouselInner)
-}
-
-this.elem = elem;
-let leftButton = elem.querySelector(".carousel__arrow_left");
-let rightButton = elem.querySelector(".carousel__arrow_right");
-leftButton.style.display = 'none';
-let allSlides = carouselInner.childNodes;
-let maxNumber = allSlides.length - 1;
-let positionLenta = 0;
-let currentSlide = allSlides[positionLenta]
-
-function clickInTheRight (){
-  let divInner = elem.querySelector(".carousel__inner");
-  let slideWidth = divInner.offsetWidth;
-  if(positionLenta<maxNumber){
-  leftButton.style.display = ''; 
-  positionLenta += 1;
-  currentSlide = allSlides[positionLenta];
-  }
-  if(positionLenta === maxNumber){
-    rightButton.style.display = 'none'
+      carouselInner.append(newSlide);
+      elem.append(carouselInner);
+    }
+    return elem;
   }
 
- divInner.style.transform = `translateX(${-slideWidth*positionLenta}px)`;
-}
-rightButton.addEventListener('click', clickInTheRight);
+  #onRightArrowClick = () => {
+    const carouselInner = this.elem.querySelector(".carousel__inner");
+    let maxNumber = this.elem.querySelectorAll(".carousel__button").length - 1;
+    let slideWidth = carouselInner.offsetWidth;
+    if (this.positionLenta < maxNumber) {
+      this.elem.querySelector(".carousel__arrow_left").style.display = "";
+      this.positionLenta += 1;
+    }
+    if (this.positionLenta === maxNumber) {
+      this.elem.querySelector(".carousel__arrow_right").style.display = "none";
+    }
+    carouselInner.style.transform = `translateX(${
+      -slideWidth * this.positionLenta
+    }px)`;
+  };
 
-function clickInTheLeft (){
-  let divInner = elem.querySelector(".carousel__inner");
-  let slideWidth = divInner.offsetWidth;
-  rightButton.style.display = '';  
-  if(positionLenta===1){
-  leftButton.style.display = 'none'
-  }
-  if(positionLenta>0){ 
-  positionLenta -= 1;
-  currentSlide = allSlides[positionLenta]
-  }
- divInner.style.transform = `translateX(${-slideWidth*positionLenta}px)`;
-}
-leftButton.addEventListener('click', clickInTheLeft);
+  #onLeftArrowClick = () => {
+    const carouselInner = this.elem.querySelector(".carousel__inner");
+    let slideWidth = carouselInner.offsetWidth;
+    this.elem.querySelector(".carousel__arrow_right").style.display = "";
+    if (this.positionLenta === 1) {
+      this.elem.querySelector(".carousel__arrow_left").style.display = "none";
+    }
+    if (this.positionLenta > 0) {
+      this.positionLenta -= 1;
+    }
+    carouselInner.style.transform = `translateX(${
+      -slideWidth * this.positionLenta
+    }px)`;
+  };
 
-function createEvent(){
-  let event = new CustomEvent("product-add", {
-   detail: currentSlide.getAttribute("data-id"),
-    bubbles: true,
-    }); 
-  elem.dispatchEvent(event)
+  #onProductAddClick = () => {
+    let currentSlide = this.elem.querySelector(".carousel__inner").childNodes[this.positionLenta];
+    console.log(currentSlide)
+    let event = new CustomEvent("product-add", {
+      detail: currentSlide.getAttribute("data-id"),
+      bubbles: true,
+    });
+    this.elem.dispatchEvent(event);
+  };
 }
-let buttons = elem.querySelectorAll(".carousel__button");
-for (let i = 0; i<buttons.length; i++) {
-  buttons[i].addEventListener('click', createEvent);
-}
-}
- }
